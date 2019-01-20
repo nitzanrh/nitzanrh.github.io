73c6cbcbc8cbbb4b4519201c50216fd78a30f27c
nitzanrh <nitzanrh@gmail.com>
2019-01-20 15:13:08 +0200

    Add files via upload

---
+++ js/demo/demo.js
@@ -0,0 +1,57 @@
+/*
+ * blueimp Gallery Demo JS
+ * https://github.com/blueimp/Gallery
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ */
+
+/* global blueimp, $ */
+
+$(function () {
+  'use strict'
+
+
+  // Initialize the Gallery as video carousel:
+  blueimp.Gallery([
+    {
+        title: 'Nitin Sawhney - The Conference (Drum Cover)',
+        href: 'https://www.youtube.com/watch?v=nJWk5YcZUHI',
+        type: 'text/html',
+        youtube: 'nJWk5YcZUHI',
+        poster: 'https://img.youtube.com/vi/nJWk5YcZUHI/maxresdefault.jpg'
+    },
+
+    {
+        title: 'Live w/ Paul Pedana @ 93 Feet East, London',
+        href: 'https://www.youtube.com/watch?v=x6hPCa7x14U',
+        type: 'text/html',
+        youtube: 'x6hPCa7x14U',
+        poster: 'https://img.youtube.com/vi/x6hPCa7x14U/maxresdefault.jpg'
+    },
+
+    {
+        title: 'Live w/ Meridian Fall at Surya, London 2016',
+        href: 'https://www.youtube.com/watch?v=_Pj1AjzEH-Q',
+        type: 'text/html',
+        youtube: '_Pj1AjzEH-Q',
+        poster: 'https://img.youtube.com/vi/_Pj1AjzEH-Q/maxresdefault.jpg'
+    },
+
+    {
+        title: 'Assaf Avidan - Her Lies - Drum Cover by Nitzan Ravhon',
+        href: 'https://www.youtube.com/watch?v=qJOA5XOJrqU',
+        type: 'text/html',
+        youtube: 'qJOA5XOJrqU',
+        poster: 'https://img.youtube.com/vi/qJOA5XOJrqU/maxresdefault.jpg'
+    },
+  ],
+
+   {
+    container: '#blueimp-video-carousel',
+    carousel: true
+  })
+})
