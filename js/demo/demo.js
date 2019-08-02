f4deb2c2fbc1fb4a8a37a09f04e007f2d504c527
nizanravhon <nitzanrh@gmail.com>
2019-08-02 10:29:37 +0300

    New website

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -17,6 +17,21 @@ $(function () {
 
   // Initialize the Gallery as video carousel:
   blueimp.Gallery([
+        {
+        title: 'Live w/ Orpheus Blade @ Gararin, Tel Aviv',
+        href: 'https://www.youtube.com/watch?v=Aif1zXGy2vU',
+        type: 'text/html',
+        youtube: 'Aif1zXGy2vU',
+        poster: 'https://img.youtube.com/vi/Aif1zXGy2vU/maxresdefault.jpg'
+    },
+      {
+        title: 'Live w/ Paul Pedana @ 93 Feet East, London',
+        href: 'https://www.youtube.com/watch?v=x6hPCa7x14U',
+        type: 'text/html',
+        youtube: 'x6hPCa7x14U',
+        poster: 'https://img.youtube.com/vi/x6hPCa7x14U/maxresdefault.jpg'
+    },
+
     {
         title: 'Nitin Sawhney - The Conference (Drum Cover)',
         href: 'https://www.youtube.com/watch?v=nJWk5YcZUHI',
@@ -25,13 +40,6 @@ $(function () {
         poster: 'https://img.youtube.com/vi/nJWk5YcZUHI/maxresdefault.jpg'
     },
 
-    {
-        title: 'Live w/ Paul Pedana @ 93 Feet East, London',
-        href: 'https://www.youtube.com/watch?v=x6hPCa7x14U',
-        type: 'text/html',
-        youtube: 'x6hPCa7x14U',
-        poster: 'https://img.youtube.com/vi/x6hPCa7x14U/maxresdefault.jpg'
-    },
 
     {
         title: 'Live w/ Meridian Fall at Surya, London 2016',
